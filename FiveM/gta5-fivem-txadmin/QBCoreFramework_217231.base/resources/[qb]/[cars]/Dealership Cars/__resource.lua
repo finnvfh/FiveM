7fa6fa21cffc3249b0e4462feac26487149ed7be
GoingLoki <goingloki@gmail.com>
2021-08-26 14:44:39 -0400

    add part of server

---
+++ FiveM/gta5-fivem-txadmin/QBCoreFramework_217231.base/resources/[qb]/[cars]/Dealership Cars/__resource.lua
@@ -0,0 +1,445 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'i8/vehicles.meta',
+    'i8/carvariations.meta',
+    'i8/carcols.meta',
+    'i8/handling.meta',
+	'x6m/vehicles.meta',
+    'x6m/carvariations.meta',
+    'x6m/carcols.meta',
+    'm5e60/handling.meta',
+	'm5e60/vehicles.meta',
+    'm5e60/carvariations.meta',
+    'm5e60/carcols.meta',
+    'm5e60/handling.meta',
+	'm5f90/vehicles.meta',
+    'm5f90/carvariations.meta',
+    'm5f90/carcols.meta',
+    'm5f90/handling.meta',
+	'bmwm8/vehicles.meta',
+    'bmwm8/carvariations.meta',
+    'bmwm8/carcols.meta',
+    'bmwm8/handling.meta',
+	'm3gts/vehicles.meta',
+    'm3gts/carvariations.meta',
+    'm3gts/carcols.meta',
+    'm3gts/handling.meta',
+	'rs7/vehicles.meta',
+    'rs7/carvariations.meta',
+    'rs7/carcols.meta',
+    'rs7/handling.meta',
+	'r8ppi/vehicles.meta',
+    'r8ppi/carvariations.meta',
+    'r8ppi/carcols.meta',
+    'r8ppi/handling.meta',
+	'sq72016/vehicles.meta',
+    'sq72016/carvariations.meta',
+    'sq72016/carcols.meta',
+    'sq72016/handling.meta',
+	'rs318/vehicles.meta',
+    'rs318/carvariations.meta',
+    'rs318/carcols.meta',
+    'rs318/handling.meta',
+	'tts/vehicles.meta',
+    'tts/carvariations.meta',
+    'tts/carcols.meta',
+    'tts/handling.meta',
+	'q820/vehicles.meta',
+    'q820/carvariations.meta',
+    'q820/carcols.meta',
+    'q820/handling.meta',
+	'rs5r/vehicles.meta',
+    'rs5r/carvariations.meta',
+    'rs5r/carcols.meta',
+    'rs5r/handling.meta',
+	'r8v10abt/vehicles.meta',
+    'r8v10abt/carvariations.meta',
+    'r8v10abt/carcols.meta',
+    'r8v10abt/handling.meta',
+	'rs6/vehicles.meta',
+    'rs6/carvariations.meta',
+    'rs6/carcols.meta',
+    'rs6/handling.meta',
+	's3sedan/vehicles.meta',
+    's3sedan/carvariations.meta',
+    's3sedan/carcols.meta',
+    's3sedan/handling.meta',
+	'cls2015/vehicles.meta',
+    'cls2015/carvariations.meta',
+    'cls2015/carcols.meta',
+    'cls2015/handling.meta',
+	'2016MercedesAMGCLA45ShootingBrake/vehicles.meta',
+    '2016MercedesAMGCLA45ShootingBrake/carvariations.meta',
+    '2016MercedesAMGCLA45ShootingBrake/carcols.meta',
+    '2016MercedesAMGCLA45ShootingBrake/handling.meta',
+	'a45/vehicles.meta',
+    'a45/carvariations.meta',
+    'a45/carcols.meta',
+    'a45/handling.meta',
+	's65w222/vehicles.meta',
+    's65w222/carvariations.meta',
+    's65w222/carcols.meta',
+    's65w222/handling.meta',
+	'amgc/vehicles.meta',
+    'amgc/carvariations.meta',
+    'amgc/carcols.meta',
+    'amgc/handling.meta',
+	'c63w205/vehicles.meta',
+    'c63w205/carvariations.meta',
+    'c63w205/carcols.meta',
+    'c63w205/handling.meta',
+	'gtr/vehicles.meta',
+    'gtr/carvariations.meta',
+    'gtr/carcols.meta',
+    'gtr/handling.meta',
+	'evo9/vehicles.meta',
+    'evo9/carvariations.meta',
+    'evo9/carcols.meta',
+    'evo9/handling.meta',
+	'fk8/vehicles.meta',
+    'fk8/carvariations.meta',
+    'fk8/carcols.meta',
+    'fk8/handling.meta',
+	'ap2/vehicles.meta',
+    'ap2/carvariations.meta',
+    'ap2/carcols.meta',
+    'ap2/handling.meta',
+	'eg6/vehicles.meta',
+    'eg6/carvariations.meta',
+    'eg6/carcols.meta',
+    'eg6/handling.meta',
+	'na6/vehicles.meta',
+    'na6/carvariations.meta',
+    'na6/carcols.meta',
+    'na6/handling.meta',
+	'fc3s/vehicles.meta',
+    'fc3s/carvariations.meta',
+    'fc3s/carcols.meta',
+    'fc3s/handling.meta',
+	'rx811/vehicles.meta',
+    'rx811/carvariations.meta',
+    'rx811/carcols.meta',
+    'rx811/handling.meta',
+	'16charger/vehicles.meta',
+    '16charger/carvariations.meta',
+    '16charger/carcols.meta',
+    '16charger/handling.meta',
+	'demon/vehicles.meta',
+    'demon/carvariations.meta',
+    'demon/carcols.meta',
+    'demon/handling.meta',
+	'18performante/vehicles.meta',
+    '18performante/carvariations.meta',
+    '18performante/carcols.meta',
+    '18performante/handling.meta',
+	'f430/vehicles.meta',
+    'f430/carvariations.meta',
+    'f430/carcols.meta',
+    'f430/handling.meta',
+	'f488/vehicles.meta',
+    'f488/carvariations.meta',
+    'f488/carcols.meta',
+    'f488/handling.meta',
+	'ssti/vehicles.meta',
+    'ssti/carvariations.meta',
+    'ssti/carcols.meta',
+    'ssti/handling.meta',
+	'600LT/vehicles.meta',
+    '600LT/carvariations.meta',
+    '600LT/carcols.meta',
+    '600LT/handling.meta',
+	'720s/vehicles.meta',
+    '720s/carvariations.meta',
+    '720s/carcols.meta',
+    '720s/handling.meta',
+	'p1/vehicles.meta',
+    'p1/carvariations.meta',
+    'p1/carcols.meta',
+    'p1/handling.meta',
+	'aventador/vehicles.meta',
+    'aventador/carvariations.meta',
+    'aventador/carcols.meta',
+    'aventador/handling.meta',
+	'675ltsp/vehicles.meta',
+    '675ltsp/carvariations.meta',
+    '675ltsp/carcols.meta',
+    '675ltsp/handling.meta',
+	'765lt/vehicles.meta',
+    '765lt/carvariations.meta',
+    '765lt/carcols.meta',
+    '765lt/handling.meta',
+	'laferrari/vehicles.meta',
+    'laferrari/carvariations.meta',
+    'laferrari/carcols.meta',
+    'laferrari/handling.meta',
+	'gcmyarisgr/vehicles.meta',
+    'gcmyarisgr/carvariations.meta',
+    'gcmyarisgr/carcols.meta',
+    'gcmyarisgr/handling.meta',
+	'veln/vehicles.meta',
+    'veln/carvariations.meta',
+    'veln/carcols.meta',
+    'veln/handling.meta',
+	'gcmm5cs/vehicles.meta',
+    'gcmm5cs/carvariations.meta',
+    'gcmm5cs/carcols.meta',
+    'gcmm5cs/handling.meta',
+	'gcmrs52018/vehicles.meta',
+    'gcmrs52018/carvariations.meta',
+    'gcmrs52018/carcols.meta',
+    'gcmrs52018/handling.meta',
+	'gcmgolf8r/vehicles.meta',
+    'gcmgolf8r/carvariations.meta',
+    'gcmgolf8r/carcols.meta',
+    'gcmgolf8r/handling.meta',
+	'gcmsubaru21ts/vehicles.meta',
+    'gcmsubaru21ts/carvariations.meta',
+    'gcmsubaru21ts/carcols.meta',
+    'gcmsubaru21ts/handling.meta',
+	'911turbos/vehicles.meta',
+    '911turbos/carvariations.meta',
+    '911turbos/carcols.meta',
+    '911turbos/handling.meta',
+	'm422/vehicles.meta',
+    'm422/carvariations.meta',
+    'm422/carcols.meta',
+    'm422/handling.meta',
+	'ctsv16/vehicles.meta',
+    'ctsv16/carvariations.meta',
+    'ctsv16/carcols.meta',
+    'ctsv16/handling.meta',
+	'ctsvc/vehicles.meta',
+    'ctsvc/carvariations.meta',
+    'ctsvc/carcols.meta',
+    'ctsvc/handling.meta',
+	'ep3/vehicles.meta',
+    'ep3/carvariations.meta',
+    'ep3/carcols.meta',
+    'ep3/handling.meta',
+	'roadster/vehicles.meta',
+    'roadster/carvariations.meta',
+    'roadster/carcols.meta',
+    'roadster/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'i8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'i8/vehicles.meta'
+data_file 'CARCOLS_FILE' 'i8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'i8/carvariations.meta'
+data_file 'HANDLING_FILE' 'x6m/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'x6m/vehicles.meta'
+data_file 'CARCOLS_FILE' 'x6m/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'x6m/carvariations.meta'
+data_file 'HANDLING_FILE' 'm5e60/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'm5e60/vehicles.meta'
+data_file 'CARCOLS_FILE' 'm5e60/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'm5e60/carvariations.meta'
+data_file 'HANDLING_FILE' 'm5f90/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'm5f90/vehicles.meta'
+data_file 'CARCOLS_FILE' 'm5f90/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'm5f90/carvariations.meta'
+data_file 'HANDLING_FILE' 'bmwm8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'bmwm8/vehicles.meta'
+data_file 'CARCOLS_FILE' 'bmwm8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'bmwm8/carvariations.meta'
+data_file 'HANDLING_FILE' 'm3gts/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'm3gts/vehicles.meta'
+data_file 'CARCOLS_FILE' 'm3gts/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'm3gts/carvariations.meta'
+data_file 'HANDLING_FILE' 'rs7/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs7/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs7/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs7/carvariations.meta'
+data_file 'HANDLING_FILE' 'r8ppi/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'r8ppi/vehicles.meta'
+data_file 'CARCOLS_FILE' 'r8ppi/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'r8ppi/carvariations.meta'
+data_file 'HANDLING_FILE' 'sq72016/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'sq72016/vehicles.meta'
+data_file 'CARCOLS_FILE' 'sq72016/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'sq72016/carvariations.meta'
+data_file 'HANDLING_FILE' 'rs318/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs318/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs318/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs318/carvariations.meta'
+data_file 'HANDLING_FILE' 'tts/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'tts/vehicles.meta'
+data_file 'CARCOLS_FILE' 'tts/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'tts/carvariations.meta'
+data_file 'HANDLING_FILE' 'q820/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'q820/vehicles.meta'
+data_file 'CARCOLS_FILE' 'q820/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'q820/carvariations.meta'
+data_file 'HANDLING_FILE' 'rs5r/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs5r/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs5r/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs5r/carvariations.meta'
+data_file 'HANDLING_FILE' 'r8v10abt/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'r8v10abt/vehicles.meta'
+data_file 'CARCOLS_FILE' 'r8v10abt/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'r8v10abt/carvariations.meta'
+data_file 'HANDLING_FILE' 'rs6/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rs6/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rs6/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rs6/carvariations.meta'
+data_file 'HANDLING_FILE' 's3sedan/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 's3sedan/vehicles.meta'
+data_file 'CARCOLS_FILE' 's3sedan/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 's3sedan/carvariations.meta'
+data_file 'HANDLING_FILE' 'cls2015/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'cls2015/vehicles.meta'
+data_file 'CARCOLS_FILE' 'cls2015/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'cls2015/carvariations.meta'
+data_file 'HANDLING_FILE' '2016MercedesAMGCLA45ShootingBrake/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '2016MercedesAMGCLA45ShootingBrake/vehicles.meta'
+data_file 'CARCOLS_FILE' '2016MercedesAMGCLA45ShootingBrake/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '2016MercedesAMGCLA45ShootingBrake/carvariations.meta'
+data_file 'HANDLING_FILE' 'a45/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'a45/vehicles.meta'
+data_file 'CARCOLS_FILE' 'a45/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'a45/carvariations.meta'
+data_file 'HANDLING_FILE' 's65w222/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 's65w222/vehicles.meta'
+data_file 'CARCOLS_FILE' 's65w222/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 's65w222/carvariations.meta'
+data_file 'HANDLING_FILE' 'amgc/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'amgc/vehicles.meta'
+data_file 'CARCOLS_FILE' 'amgc/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'amgc/carvariations.meta'
+data_file 'HANDLING_FILE' 'c63w205/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'c63w205/vehicles.meta'
+data_file 'CARCOLS_FILE' 'c63w205/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'c63w205/carvariations.meta'
+data_file 'HANDLING_FILE' 'gtr/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gtr/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gtr/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'gtr/carvariations.meta'
+data_file 'HANDLING_FILE' 'evo9/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'evo9/vehicles.meta'
+data_file 'CARCOLS_FILE' 'evo9/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'evo9/carvariations.meta'
+data_file 'HANDLING_FILE' 'fk8/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'fk8/vehicles.meta'
+data_file 'CARCOLS_FILE' 'fk8/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'fk8/carvariations.meta'
+data_file 'HANDLING_FILE' 'ap2/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ap2/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ap2/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ap2/carvariations.meta'
+data_file 'HANDLING_FILE' 'eg6/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'eg6/vehicles.meta'
+data_file 'CARCOLS_FILE' 'eg6/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'eg6/carvariations.meta'
+data_file 'HANDLING_FILE' 'na6/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'na6/vehicles.meta'
+data_file 'CARCOLS_FILE' 'na6/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'na6/carvariations.meta'
+data_file 'HANDLING_FILE' 'fc3s/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'fc3s/vehicles.meta'
+data_file 'CARCOLS_FILE' 'fc3s/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'fc3s/carvariations.meta'
+data_file 'HANDLING_FILE' 'rx811/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'rx811/vehicles.meta'
+data_file 'CARCOLS_FILE' 'rx811/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'rx811/carvariations.meta'
+data_file 'HANDLING_FILE' '16charger/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '16charger/vehicles.meta'
+data_file 'CARCOLS_FILE' '16charger/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '16charger/carvariations.meta'
+data_file 'HANDLING_FILE' 'demon/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'demon/vehicles.meta'
+data_file 'CARCOLS_FILE' 'demon/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'demon/carvariations.meta'
+data_file 'HANDLING_FILE' '18performante/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '18performante/vehicles.meta'
+data_file 'CARCOLS_FILE' '18performante/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '18performante/carvariations.meta'
+data_file 'HANDLING_FILE' 'f430/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'f430/vehicles.meta'
+data_file 'CARCOLS_FILE' 'f430/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'f430/carvariations.meta'
+data_file 'HANDLING_FILE' 'f488/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'f488/vehicles.meta'
+data_file 'CARCOLS_FILE' 'f488/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'f488/carvariations.meta'
+data_file 'HANDLING_FILE' 'ssti/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ssti/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ssti/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ssti/carvariations.meta'
+data_file 'HANDLING_FILE' '600LT/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '600LT/vehicles.meta'
+data_file 'CARCOLS_FILE' '600LT/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '600LT/carvariations.meta'
+data_file 'HANDLING_FILE' '720s/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '720s/vehicles.meta'
+data_file 'CARCOLS_FILE' '720s/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '720s/carvariations.meta'
+data_file 'HANDLING_FILE' 'p1/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'p1/vehicles.meta'
+data_file 'CARCOLS_FILE' 'p1/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'p1/carvariations.meta'
+data_file 'HANDLING_FILE' 'aventador/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'aventador/vehicles.meta'
+data_file 'CARCOLS_FILE' 'aventador/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'aventador/carvariations.meta'
+data_file 'HANDLING_FILE' '675ltsp/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '675ltsp/vehicles.meta'
+data_file 'CARCOLS_FILE' '675ltsp/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '675ltsp/carvariations.meta'
+data_file 'HANDLING_FILE' '765lt/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '765lt/vehicles.meta'
+data_file 'CARCOLS_FILE' '765lt/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '765lt/carvariations.meta'
+data_file 'HANDLING_FILE' 'laferrari/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'laferrari/vehicles.meta'
+data_file 'CARCOLS_FILE' 'laferrari/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'laferrari/carvariations.meta'
+data_file 'HANDLING_FILE' 'gcmyarisgr/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gcmyarisgr/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gcmyarisgr/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'gcmyarisgr/carvariations.meta'
+data_file 'HANDLING_FILE' 'veln/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'veln/vehicles.meta'
+data_file 'CARCOLS_FILE' 'veln/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'veln/carvariations.meta'
+data_file 'HANDLING_FILE' 'gcmm5cs/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gcmm5cs/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gcmm5cs/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'gcmm5cs/carvariations.meta'
+data_file 'HANDLING_FILE' 'gcmrs52018/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gcmrs52018/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gcmrs52018/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'gcmrs52018/carvariations.meta'
+data_file 'HANDLING_FILE' 'gcmgolf8r/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gcmgolf8r/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gcmgolf8r/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'gcmgolf8r/carvariations.meta'
+data_file 'HANDLING_FILE' 'gcmsubaru21ts/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'gcmsubaru21ts/vehicles.meta'
+data_file 'CARCOLS_FILE' 'gcmsubaru21ts/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'cgcmsubaru21ts/arvariations.meta'
+data_file 'HANDLING_FILE' '911turbos/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' '911turbos/vehicles.meta'
+data_file 'CARCOLS_FILE' '911turbos/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' '911turbos/carvariations.meta'
+data_file 'HANDLING_FILE' 'm422/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'm422/vehicles.meta'
+data_file 'CARCOLS_FILE' 'm422/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'm422/carvariations.meta'
+data_file 'HANDLING_FILE' 'ctsv16/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ctsv16/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ctsv16/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ctsv16/carvariations.meta'
+data_file 'HANDLING_FILE' 'ctsvc/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ctsvc/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ctsvc/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ctsvc/carvariations.meta'
+data_file 'HANDLING_FILE' 'ep3/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'ep3/vehicles.meta'
+data_file 'CARCOLS_FILE' 'ep3/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'ep3/carvariations.meta'
+data_file 'HANDLING_FILE' 'roadster/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'roadster/vehicles.meta'
+data_file 'CARCOLS_FILE' 'roadster/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'roadster/carvariations.meta'
