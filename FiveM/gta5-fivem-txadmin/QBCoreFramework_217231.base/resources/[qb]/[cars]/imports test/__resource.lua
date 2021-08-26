8636607e36bba6ac3018830283a2b49019f7ccae
GoingLoki <goingloki@gmail.com>
2021-08-26 15:16:14 -0400

    add part of server

---
+++ FiveM/gta5-fivem-txadmin/QBCoreFramework_217231.base/resources/[qb]/[cars]/imports test/__resource.lua
@@ -0,0 +1,13 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+ 
+files {
+    'mustang/vehicles.meta',
+    'mustang/carvariations.meta',
+    'mustang/carcols.meta',
+    'mustang/handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'mustang/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'mustang/vehicles.meta'
+data_file 'CARCOLS_FILE' 'mustang/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'mustang/carvariations.meta'
