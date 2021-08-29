cf70ec505982a9fc2e1ea544ef7a57e694e7ccea
Tranqi <rehmanprive@gmail.com>
2021-08-29 04:11:23 +0200

    more folders moved

---
+++ FiveM/gta5-fivem-txadmin/QBCoreFramework_217231.base/resources/[qb]/[mlo]/CustomProps/fxmanifest.lua
@@ -0,0 +1,23 @@
+description 'UKOH Custom Props'
+name 'UKOH Custom Props'
+
+fx_version 'adamant'
+games {'gta5'}
+
+files {
+    'stream/axondock.ytyp',
+	'stream/sinnersign.ytyp',
+	'stream/metsign.ytyp',
+	'stream/policelantern.ytyp',
+	'stream/aed.ytyp',
+	'stream/firstaidkit.ytyp',
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/axondock.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/sinnersign.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/metsign.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/policelantern.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/aed.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/firstaidkit.ytyp'
+
+this_is_a_map 'yes'
