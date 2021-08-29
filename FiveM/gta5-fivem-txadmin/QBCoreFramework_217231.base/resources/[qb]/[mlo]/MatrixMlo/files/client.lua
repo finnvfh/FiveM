830cc243587eefa01e86b82e7e8c4d9222bccb57
Tranqi <rehmanprive@gmail.com>
2021-08-29 04:32:52 +0200

    mlo 3

---
+++ FiveM/gta5-fivem-txadmin/QBCoreFramework_217231.base/resources/[qb]/[mlo]/MatrixMlo/files/client.lua
@@ -0,0 +1,13 @@
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+local tun = GetInteriorAtCoordsWithType(1176.157, -3193.703, -9.278,"uj_tuner_car_meet2")-- Leaked By: Leaking Hub | J. Snow | leakinghub.com 
+EnableInteriorProp(tun, "set1") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set2") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set3") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set4") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set5") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set6") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set7") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set8") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+EnableInteriorProp(tun, "set9") ---- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+RefreshInterior(tun)
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
